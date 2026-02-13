02f39be96bc1f0d019272e702fb4d2082c141f7a
Viviana <dvivianapaty@gmail.com>
2026-02-12 21:48:18 -0500

    v3

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
-from fasapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+#CORS: Para habilitar peticiones de clientes que no están en mi dominio
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials= True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 @app.get("/sumar")
 def sumar_numeros(a:float, b:float):
     return a+b
+
+
+#python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload.\
